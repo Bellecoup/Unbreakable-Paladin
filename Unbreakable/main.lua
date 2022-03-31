16738ea8a0d30d7cb4bc6c5a82965fa8aa561f57
Bellecoup <102752113+Bellecoup@users.noreply.github.com>
2022-03-31 11:16:05 -0500

    Add files via upload
    Adding in documentation with comments

--- Unbreakable/main.lua
+++ Unbreakable/main.lua
@@ -4,9 +4,13 @@ EventFrame:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
 EventFrame:SetScript("OnEvent", function(self, event, ...)
 	
 	if(event == "PLAYER_EQUIPMENT_CHANGED") then
+	--Initializing flag to check if Holy Shield buff is active
 	HShield = false
+		--Loop to check all active buffs
 		for x=1,40,1 do
+			--Fetching information on currently selected buff
 			local _,_,_,_,_,_,_,_,_,s= UnitAura("player", x)
+			--Checking if the current buff is Holy Shield
 			if(s==27179) then
 				HShield = true
 				break
@@ -14,7 +18,7 @@ EventFrame:SetScript("OnEvent", function(self, event, ...)
 				HShield = false
 			end
 		end
-			
+		--Calculating effective avoidance based on whether or not Holy Shield is currently active and displaying results as if it were.	
 		if(HShield==true) then
 			print("Avoidance is at:", GetDodgeChance()+GetBlockChance()+GetParryChance()+5+(GetCombatRating(CR_DEFENSE_SKILL)*150/355+20)*0.04)
 		else
